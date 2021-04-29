eb0c41d6d03db26b8e37642c75f6438e6865478e
3678369 <momar.etu.upmc.fr>
2021-04-29 16:27:10 +0200

    structure du projet

---
+++ src/Initialization.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Initialization {
+
+	private final static int N = 4;
+	
+	public static void main(String args[]) {
+		
+	}
+}
